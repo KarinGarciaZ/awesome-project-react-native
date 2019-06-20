9e3da134a088eabc7a3fd623e7f8d98ff3d24903
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-19 20:37:07 -0500

    Added reducer of places for redux.

---
+++ src/store/reducers/places.js
@@ -0,0 +1,39 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
+const reducer = ( state, action ) => {
+  switch( action.type ) {
+    case ADD_PLACE:   
+      let newPlaces = [...state.places]   
+      newPlaces.push({
+        key: Math.random(), 
+        value: action.payload, 
+        image: {
+          uri:'https://cdn.muenchen-p.de/.imaging/stk/responsive/galleryLarge/dms/shutterstock/neues-rathaus-marienplatz/document/neues-rathaus-marienplatz.jpg'
+        }
+      })
+      return {
+        ...state, 
+        places: newPlaces
+      };
+    case DELETE_PLACE:      
+      return {
+        ...state, 
+        places: state.places.filter(item => item.key !== state.selectedPlace.key ),
+        selectedPlace: null
+      };
+    case SELECT_PLACE:      
+      return {
+        ...state, 
+        selectedPlace: state.places.find( item => item.key === action.payload )
+      };
+    case DESELECT_PLACE:      
+      return {
+        ...state, 
+        selectedPlace: null
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer
