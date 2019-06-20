a4c021595df8690618444369feca5cfdbb98cc96
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-19 20:12:27 -0500

    Context and reducer initializated.

--- App.js
+++ App.js
@@ -1,47 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { StyleSheet, View, } from 'react-native';
 import Items from './src/components/Items/items';
 import AddPlaces from './src/components/Form/AddPlaces';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+
+import PlacesContext from './src/store/contexts/places';
+import placesReducer from './src/store/reducers/places';
+
 export default App = () => {
 
-  let [inputArr, setInputArr] = useState([])
-  let [showModal, setShowModal] = useState(null)
-
-  const onClickButton = (value) => {
-    let temp = [...inputArr];
-    temp.push({
-      key: Math.random(), 
-      value, 
-      image: {
-        uri:'https://cdn.muenchen-p.de/.imaging/stk/responsive/galleryLarge/dms/shutterstock/neues-rathaus-marienplatz/document/neues-rathaus-marienplatz.jpg'
-      }
-    })
-    setInputArr( temp )
+  const initialState = {
+    places: [],
+    selectedPlace: null
   }
+  const placesContextValue = useReducer( placesReducer, initialState )
 
-  const onPressItem = (key) => {
-    let temp = [...inputArr];
-    let showModalEl = temp.find(item => item.key === key )
-    setShowModal( showModalEl )
-  }
-
-  const onDeleteItem = (key) => {
-    let temp = [...inputArr];
-    temp = temp.filter(item => item.key !== key )
-    setInputArr( [...temp] )
-    setShowModal( null )
-  }
-
-  const onCloseModal = () => {
-    setShowModal( null )
-  }
-  
   return (
     <View style={styles.container}>
-      <PlaceDetail itemToShow={showModal} onClose={onCloseModal} onDelete={onDeleteItem} />
-      <AddPlaces clicked={onClickButton} />
-      <Items items={inputArr} pressItem={onPressItem}></Items>
+      <PlacesContext.Provider value={placesContextValue}>
+        <PlaceDetail />
+        <AddPlaces />
+        <Items />
+      </PlacesContext.Provider>
     </View>
   );
 }
