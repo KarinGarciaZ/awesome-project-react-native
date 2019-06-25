9371b06c2d80baff1c30c343d25d94ed12c09d58
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-24 19:51:19 -0500

    Removed current code to Home.

--- App.js
+++ App.js
@@ -1,29 +1,9 @@
 import React, { useState, useReducer } from 'react';
-import { StyleSheet, View, } from 'react-native';
-import Items from './src/components/Items/items';
-import AddPlaces from './src/components/Form/AddPlaces';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-
-import PlacesProvider from './src/store/providers/placesProvider';
+import AppContainer from './navigation'
 
 export default App = () => {
 
   return (
-    <View style={styles.container}>
-      <PlacesProvider>
-        <PlaceDetail />
-        <AddPlaces />
-        <Items />
-      </PlacesProvider>
-    </View>
+    <AppContainer />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
