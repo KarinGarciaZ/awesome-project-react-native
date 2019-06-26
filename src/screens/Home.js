6f6a3db8f6aca3bb6f89b4cf13f9b63eb8e4c3b1
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-25 20:30:49 -0500

    Added custom header.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Items from '../components/Items/items';
 import AddPlaces from '../components/Form/AddPlaces';
-import PlaceDetail from '../components/PlaceDetail/PlaceDetail';
-import PlacesProvider from '../store/providers/placesProvider';
+import Header from '../components/Shared/Header';
 
-export default App = () => {
+export default Home = props => {
+
+  const onPressMoreAboutUs = () => {
+    props.navigation.navigate('About', { name:'karin' })
+  }
 
   return (
     <View style={styles.container}>
-      <PlacesProvider>
-        <PlaceDetail />
-        <AddPlaces />
-        <Items />
-      </PlacesProvider>
+      <Header>
+        <Icon size={25} name="instagram" color='black' />
+        <Text>Home</Text>
+        <Icon size={20} name="ellipsis-v" color='black' />
+      </Header>
+      <AddPlaces />
+      <Items {...props}/>
+      <Button title='ccc' onPress={() => props.navigation.navigate('About')} />
     </View>
   );
 }
