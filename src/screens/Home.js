cb48bfc881acdb8d211ec0e8db12a9938cb891c2
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-06-26 22:58:40 -0500

    Added implementation to add places to header.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { StyleSheet, View, Button, Text, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Items from '../components/Items/items';
 import AddPlaces from '../components/Form/AddPlaces';
 import Header from '../components/Shared/Header';
+import ContextPlaces from '../store/contexts/placesContext';
+import { addPlace } from '../store/actions/placesActions';
 
 export default Home = props => {
+
+  let [search, setSearch] = useState('');
+  let [state, dispatch] = useContext(ContextPlaces)
+
+  const onAddPlace = () => {
+    if( search.trim() !== '' )
+      dispatch(addPlace(search))
+  }
 
   const onPressMoreAboutUs = () => {
     props.navigation.navigate('About', { name:'karin' })
@@ -16,12 +26,15 @@ export default Home = props => {
     <View style={styles.container}>
       <Header>
         <Icon size={25} name="instagram" color='black' />
-        <Text>Home</Text>
-        <Icon size={20} name="ellipsis-v" color='black' />
+        <View style={styles.searchInputContainer}>
+          <TextInput style={styles.searchInput} placeholder="Search" value={search} onChangeText={ value => setSearch(value) } />
+          <TouchableOpacity onPress={onAddPlace}>
+            <Icon size={20} name="search" color='#444' style={styles.searchIcon}/>
+          </TouchableOpacity>
+        </View>        
+        <Icon size={20} name="ellipsis-v" color='black' style={{paddingLeft: 15, paddingRight: 5}}/>
       </Header>
-      <AddPlaces />
       <Items {...props}/>
-      <Button title='ccc' onPress={() => props.navigation.navigate('About')} />
     </View>
   );
 }
@@ -33,4 +46,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  searchInputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 30,
+    alignSelf: 'stretch',
+    marginLeft: 25,
+    marginRight: 20,    
+    paddingLeft: 20,
+    backgroundColor: '#EEE',
+    paddingTop: 5,
+    paddingBottom: 5,    
+    borderRadius: 50
+  },
+  searchInput: {
+    flex: 1,
+    backgroundColor: 'transparent',    
+    paddingTop: 3,
+    paddingBottom: 3,
+  },
+  searchIcon: {
+    marginRight: 10
+  }
 });
