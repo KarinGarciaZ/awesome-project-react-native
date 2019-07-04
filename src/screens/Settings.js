d2fc3720ee76656611f5af5521b20eb6223c46ee
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-04 17:09:04 -0500

    added action in settings screen to go back.

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,13 +1,34 @@
-import { StyleSheet, Button, Image, } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Header from '../components/Shared/Header';
 
 const Settings = props => {
+
+  const goBack = () => {
+    props.navigation.goBack();
+  }
+
   return (
-    <Button
-      onPress={() => props.navigation.toggleDrawer()}
-      title="Go to notifications"
-    />
+    <View>
+      <Header>
+        <TouchableOpacity onPress={goBack}>
+          <Icon size={20} name="arrow-left" color='#444' style={styles.icon}/>
+        </TouchableOpacity>
+        <Text style={{fontSize: 18}}>Settings</Text>
+        <View></View>
+      </Header>
+      <Text>This is the settings screen</Text>
+    </View>  
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    paddingLeft: 5,
+    paddingRight: 10,
+  }
+})
 
 export default Settings;
