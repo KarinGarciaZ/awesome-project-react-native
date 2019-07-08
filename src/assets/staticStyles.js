854fd39558f31bccd649450540662e9af67a3a2a
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-08 18:01:26 -0500

    New colors added.

--- src/assets/staticStyles.js
+++ src/assets/staticStyles.js
@@ -1,6 +1,8 @@
 export const Colors = {
   grayOne: '#EEE',
+  garyDark: '#333',
   grayBackground: '#DDD',
   grayTransparent: '#11111111',
   grayTransparent2: '#11111155',
+  orangered: 'orangered'
 }
