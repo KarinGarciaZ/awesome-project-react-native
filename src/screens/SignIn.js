f8b2409703ea05aa9b4e1f8ad5203e1f5cd27410
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-09 13:45:43 -0500

    Added state to create a from.

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text, ScrollView, TextInput, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -9,9 +9,140 @@ import ButtonAuth from '../components/Shared/Authenticate/button';
 
 const SignIn = (props) => {
 
+  let [ formState, setFormState ] = useState({
+    controls: {
+      firstname: {
+        value: '',
+        iconName:'user',
+        placeholder:'Your first name...',
+        valid: false,
+        type: 'text',
+        validation: {
+          required: true
+        }
+      },
+      lastname: {
+        value: '',
+        iconName:'user',
+        placeholder:'Your last name...',
+        valid: false,
+        type: 'text',
+        validation: {
+          required: true
+        }
+      },
+      email: {
+        value: '',
+        iconName:'envelope',
+        placeholder:'Your email address...',
+        valid: false,
+        type: 'text',
+        validation: {
+          isEmail: true
+        }
+      },
+      password: {
+        value: '',
+        iconName:'lock',
+        placeholder:'Password...',
+        valid: false,
+        type: 'password',
+        validation: {
+          minLength: 6,
+          maxLength: 12
+        }
+      },
+      confirmPassword: {
+        value: '',
+        iconName:'lock',
+        placeholder:'Confirm password...',
+        valid: false,
+        type: 'password',
+        validation: {
+          minLength: 6,          
+          maxLength: 12,
+          equalTo: 'password'
+        }
+      }
+    },
+    valid: false
+  })
+
   const logged = () => {
     props.navigation.navigate('Home')
   }
+
+  const validateField = ( key, control ) => {
+
+    let isValid = 0;
+
+    if( control.validation.required ) {
+      if( control.value === '' )
+        isValid++;
+    }
+
+    if( control.validation.minLength ) {
+      if( control.value.length < 6 )
+        isValid++;
+    }
+
+    if( control.validation.minLength ) {
+      if( control.value.length > 12 )
+        isValid++;
+    }
+
+    if( control.validation.isEmail ) {
+      let re = /\S+@\S+\.\S+/;
+      if( !re.test(control.value) )
+        isValid++;
+    }
+
+    if( control.validation.equalTo ) {
+      if( control.value !== formState.controls[control.validation.equalTo].value )
+        isValid++;
+    }
+
+    return !isValid;
+  }
+
+  const validateForm = ( formCopy ) => {
+    
+    let formValid = true;
+
+    for( let key in formCopy.controls ) {      
+      if (!formCopy.controls[key].valid)
+        formValid = false;
+    }
+
+    return formValid;
+  }
+
+  const onChangeValue = ( key, value ) => {
+    let formStateCopy = { ...formState }    
+    formStateCopy.controls[key].value = value;
+
+    formStateCopy.controls[key].valid = validateField( key, formStateCopy.controls[key] )
+    formStateCopy.valid = validateForm(formStateCopy)
+
+    setFormState( formStateCopy )
+  }
+
+  let controls = []
+  for( let key in formState.controls ) {
+    controls.push({ key, control: formState.controls[key] })
+  }
+
+  let inputs = controls.map( element => {
+    return <Input 
+      iconName={element.control.iconName} 
+      placeholder={element.control.placeholder} 
+      value={element.control.value} 
+      valueChanged={ ( value ) => onChangeValue( element.key, value ) }
+      isPassword={element.control.type === 'password'}
+      key={element.key}
+      valid={element.control.valid}
+    />
+  })
 
   return(
     <ImageBackground source={backgroundImage} style={{width: '100%', flex: 1}}>
@@ -23,13 +154,9 @@ const SignIn = (props) => {
 
             <Text style={{...styles.textForm, marginBottom: 35}}>Create Account</Text>
 
-            <Input iconName="user" placeholder="Your first name..."/>
-            <Input iconName="user" placeholder="Your last name..."/>
-            <Input iconName="envelope" placeholder="Your email address..."/>
-            <Input iconName="lock" placeholder="Password..."/>
-            <Input iconName="lock" placeholder="Confirm password..."/>      
+            {inputs}   
 
-            <ButtonAuth text="Sign in" pressed={logged}/>
+            <ButtonAuth text="Sign in" pressed={logged} able={formState.valid}/>
 
           </View>   
         </ScrollView>         
