31a0efdba3b80c18d8f26d278d6626156771912b
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-07-12 10:19:00 -0500

    Added map to de detail.

--- src/screens/PlaceDetail.js
+++ src/screens/PlaceDetail.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity, Dimensions, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import MapView, { Marker } from 'react-native-maps';
 
 import PlaceContext from '../store/contexts/placesContext';
 import { deletePlace, deselectPlace } from '../store/actions/placesActions';
@@ -9,7 +10,7 @@ import Header from '../components/Shared/Header';
 
 const placeDetail = props => {
   
-  const [state, dispatch] = useContext(PlaceContext);
+  const [{selectedPlace}, dispatch] = useContext(PlaceContext);
   const [orientation, setOrientation] = useState(Dimensions.get('window').height > 500? 'Portrait': 'Landscape')
 
   useEffect(() => {
@@ -28,7 +29,7 @@ const placeDetail = props => {
   }
 
   const onDelete = () => {
-    dispatch(deletePlace(state.selectedPlace.key))
+    dispatch(deletePlace(selectedPlace.key))
   }
 
   const onBack = () => {
@@ -37,7 +38,7 @@ const placeDetail = props => {
 
   let screen = null
 
-  if( state.selectedPlace )  
+  if( selectedPlace )  
     screen = (    
       <View>
         <Header>
@@ -49,9 +50,9 @@ const placeDetail = props => {
         </Header>
         <ScrollView>
           <View style={orientation === 'Landscape'? styles.containerLandscape: styles.containerPortrait}>
-            <Image source={state.selectedPlace.image} style={orientation === 'Landscape'? styles.imageLandscape: styles.imagePortrait}/>
+            <Image source={selectedPlace.image} style={orientation === 'Landscape'? styles.imageLandscape: styles.imagePortrait}/>
             <View style={orientation === 'Landscape'? {flex:2}: {}}>
-              <Text style={styles.title}>{state.selectedPlace.value}</Text>
+              <Text style={styles.title}>{selectedPlace.value}</Text>
               <View style={styles.buttonContainer}>
                 <TouchableOpacity onPress={onDelete} style={styles.button}>
                   <Icon color='red' size={30} name="trash" />
@@ -61,6 +62,9 @@ const placeDetail = props => {
                 </TouchableOpacity>
               </View>
             </View>
+            <MapView style={styles.map} initialRegion={selectedPlace.location}>
+              <Marker coordinate={selectedPlace.location} />
+            </MapView>
           </View>
         </ScrollView>        
       </View>
@@ -74,7 +78,8 @@ const placeDetail = props => {
 
 const styles = StyleSheet.create({
   containerPortrait: {
-    margin: 15,    
+    margin: 15, 
+    flex: 1,   
   },
   containerLandscape: {
     margin: 15,
@@ -108,6 +113,12 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     padding: 5
+  },
+  map: {
+    width: '100%', 
+    height: 250,
+    marginTop: 15,
+    marginBottom: 50,
   }
 })
 
